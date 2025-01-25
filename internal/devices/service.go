21e000273ff02bb5e34dacfa203ac8be2cedce26
Diogo Assereuy <dio.vasc9537@gmail.com>
2025-01-24 23:30:20 -0300

    devices behaviour service definition

---
+++ internal/devices/service.go
@@ -0,0 +1,34 @@
+package devices
+
+import "fmt"
+
+func (d *Device) ChangeDeviceState(ds DeviceState) error {
+	d.State = ds
+	return nil
+}
+
+func (d *Device) ChangeDeviceName(n string) error {
+	if d.State == InUse {
+		return fmt.Errorf("trying to change device %d name while in use", d.Id)
+	}
+
+	d.Name = n
+	return nil
+}
+
+func (d *Device) ChangeDeviceBrand(b string) error {
+	if d.State == InUse {
+		return fmt.Errorf("trying to change device %d brand while in use", d.Id)
+	}
+
+	d.Brand = b
+	return nil
+}
+
+func (d *Device) CreationTimeFormatted() string {
+	return d.CreatedAt.Format(dateTimeApiLayout)
+}
+
+func (d *Device) DeviceInUse() bool {
+	return d.State == InUse
+}
