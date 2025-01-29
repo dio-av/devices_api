7d761fb68e4f9973c5bd2ec966e21665ee997659
Diogo Assereuy <dio.vasc9537@gmail.com>
2025-01-29 19:17:01 -0300

    added generic and validation error responses for the rest api renderer

--- internal/server/rest/renderes.go
+++ internal/server/rest/renderes.go
@@ -1,5 +1,27 @@
 package rest
 
-// TODO:
-// Error response payloads & renderers
 // ErrResponse renderer type for handling all sorts of errors.
+// A GenericError is the default error message that is generated.
+// For certain status codes there are more appropriate error structures.
+//
+// swagger:response genericError
+type GenericError struct {
+	// in: body
+	Body struct {
+		Code    int32 `json:"code"`
+		Message error `json:"message"`
+	} `json:"body"`
+}
+
+// A ValidationError is an error that is generated for validation failures.
+// It has the same fields as a generic error but adds a Field property.
+//
+// swagger:response validationError
+type ValidationError struct {
+	// in: body
+	Body struct {
+		Code    int32  `json:"code"`
+		Message string `json:"message"`
+		Field   string `json:"field"`
+	} `json:"body"`
+}
