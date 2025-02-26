e0e278e9e85589f3c6639e7b44f6108a237f086d
Diogo Assereuy <dio.vasc9537@gmail.com>
2025-02-26 13:10:57 -0300

    definition for payload responses for multiple and single device

--- internal/server/rest/payload.go
+++ internal/server/rest/payload.go
@@ -2,20 +2,12 @@ package rest
 
 import "devices_api/internal/devices"
 
-// TODO:
-// Request and Response payloads for the REST api.
-
-// TODO:
-// Request payload for Device data model.
-
-// TODO:
-// Response payload for the Device data model.
 type DevicesResponse struct {
-	device     []devices.Device
-	statusCode int
+	Devices    []devices.Device `json:"devices"`
+	StatusCode int              `json:"status_code"`
 }
 
 type SingleDeviceResponse struct {
-	device     devices.Device
-	statusCode int
+	Device     devices.Device `json:"device"`
+	StatusCode int            `json:"status_code"`
 }
